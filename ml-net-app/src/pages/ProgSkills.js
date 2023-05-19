0d6f83aece23ad2cb2844de06cd9bd640fba8002
Andre Enriquez <andre.enriquez@cit.edu>
2023-05-20 01:44:02 +0800

    Finalize progskills links

--- ml-net-app/src/pages/ProgSkills.js
+++ ml-net-app/src/pages/ProgSkills.js
@@ -21,8 +21,20 @@ function ProgSkills() {
         Programming Skills
         <p className='desc'>
           Programming Skills refer to the ability to write, understand, and analyze code in one or more programming languages. It comprises competence in the fundamental ideas of programming, such as variables, control flow, data structures, algorithms, and the ideas of object-oriented programming. Knowledge of syntax, libraries, frameworks, debugging strategies, and problem-solving skills are all part of programming skills. A person's ability to program allows them to construct software solutions, automate processes, manage data, and make effective and scalable applications across multiple platforms and domains.
-          <h3>Not sure about your skills? {'  '}
-            <a href="https://example.com" target="_blank" rel="noopener noreferrer" className="gradient-link">Assess yourself here!</a>
+          <h3>Not sure about your skills? {'  '} Choose your preffered language
+            <br />
+            <br />
+            <a href="https://www.linkedin.com/skill-assessments/C%20(Programming%20Language)/quiz-intro/" target="_blank" rel="noopener noreferrer" className="gradient-link">C Programming Language</a>
+            <br />
+            <a href="https://www.linkedin.com/skill-assessments/C++/quiz-intro/" target="_blank" rel="noopener noreferrer" className="gradient-link">C++ Programming Language</a>
+            <br />
+            <a href="https://www.linkedin.com/skill-assessments/C%23/quiz-intro/" target="_blank" rel="noopener noreferrer" className="gradient-link">C# Programming Language</a>
+            <br />
+            <a href="https://www.linkedin.com/skill-assessments/Java/quiz-intro/" target="_blank" rel="noopener noreferrer" className="gradient-link">Java</a>
+            <br />
+            <a href="https://www.linkedin.com/skill-assessments/JavaScript/quiz-intro/" target="_blank" rel="noopener noreferrer" className="gradient-link">JavaScript</a>
+            <br />
+            <a href="https://www.linkedin.com/skill-assessments/Python%20(Programming%20Language)/quiz-intro/" target="_blank" rel="noopener noreferrer" className="gradient-link">Python</a>
           </h3>
           <div className='rating'>
             <input
