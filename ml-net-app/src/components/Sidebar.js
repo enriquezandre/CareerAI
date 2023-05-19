671b1e95e0767d4a5593dd967ae84f4c47034554
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-05-20 00:44:37 +0800

    wtvr

--- ml-net-app/src/components/Sidebar.js
+++ ml-net-app/src/components/Sidebar.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import '../App.css'
-import './Sidebar.css'
-import {SidebarData} from './SidebarData'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
+import { useNavigate } from 'react-router-dom';
+import './Sidebar.css';
 
 function Sidebar() {
+  const location = useLocation();// then inside your component:
+  const navigate = useNavigate();
+
   return (
     <div className='Sidebar'>
       <ul className='SidebarList'>
         {SidebarData.map((val, key) => {
           return (
-          <li 
-          key={key} 
-          className='row'
-          id={window.location.pathname === val.link ? "active" : ""}
-          onClick={() => {window.location.pathname = val.link}}>
-            {" "}
-          <div id='icon'>{val.icon}</div> {" "}
-          <div id='title'> {val.title} </div>
-          </li>
+            <li 
+              key={key} 
+              className='row'
+              id={location.pathname === val.link ? "active" : ""}
+              onClick={() => {
+                // Navigate to the new page without causing a full page reload
+                navigate(val.link);
+              }}
+            >
+              <div id='icon'>{val.icon}</div> 
+              <div id='title'> {val.title} </div>
+            </li>
           );
         })}
       </ul>
@@ -25,4 +32,4 @@ function Sidebar() {
   );
 }
 
-export default Sidebar
+export default Sidebar;
